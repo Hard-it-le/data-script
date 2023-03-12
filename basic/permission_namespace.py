69ed5286bbf76c3dda5dcf50313318bc3ef585a7
yujiale <yu18371919563@163.com>
2023-03-12 17:14:18 +0800

    feat:新增权限空间基础方法

--- basic/permission_namespace.py
+++ basic/permission_namespace.py
@@ -6,7 +6,7 @@ from basic import init
 
 
 def create_permission_namespace(name=None, code=None, description=None, appid=None):
-    url = "%s/api/v3/create-permission-namespace" % (init.baseUrl)
+    url = "%s/api/v3/create-permission-namespace" % init.baseUrl
 
     payload = json.dumps({
         "name": name,
@@ -30,3 +30,74 @@ def create_permission_namespace(name=None, code=None, description=None, appid=No
     if res.json()["statusCode"] == 200:
         return res.json()["data"]
 
+
+def delete_permission_namespace(code=None):
+    url = "%s/api/v3/delete-permission-namespace" % init.baseUrl
+
+    payload = json.dumps({
+        "code": code,
+    })
+
+    headers = {
+        'x-authing-userpool-id': init.userpoolId,
+        'Authorization': init.token,
+        'Content-Type': 'application/json'
+    }
+    begin = time.time()
+    res = requests.request("POST", url, headers=headers, data=payload)
+    print(time.time() - begin)
+    print(res)
+    if res.status_code != 200:
+        return res
+    print(res.json())
+
+    if res.json()["statusCode"] == 200:
+        return res.json()["data"]
+
+
+def get_permission_namespace(code=None):
+    url = "%s/api/v3/get-permission-namespace" % init.baseUrl
+
+    query = {
+        "code": code
+    }
+
+    headers = {
+        'x-authing-userpool-id': init.userpoolId,
+        'Authorization': init.token,
+        'Content-Type': 'application/json'
+    }
+    begin = time.time()
+    res = requests.request("GET", url, headers=headers, query=query)
+    print(time.time() - begin)
+    print(res)
+    if res.status_code != 200:
+        return res
+    print(res.json())
+
+    if res.json()["statusCode"] == 200:
+        return res.json()["data"]
+
+
+def list_permission_namespace_roles(code=None):
+    url = "%s/api/v3/list-permission-namespace-roles" % init.baseUrl
+
+    query = {
+        "code": code
+    }
+
+    headers = {
+        'x-authing-userpool-id': init.userpoolId,
+        'Authorization': init.token,
+        'Content-Type': 'application/json'
+    }
+    begin = time.time()
+    res = requests.request("GET", url, headers=headers, query=query)
+    print(time.time() - begin)
+    print(res)
+    if res.status_code != 200:
+        return res
+    print(res.json())
+
+    if res.json()["statusCode"] == 200:
+        return res.json()["data"]
